cb795175f02e092af25fda33586cc066ca82e4a9
Christopher Giordano <chrisgcpg@gmail.com>
2024-06-14 22:25:00 -0400

    Added models, seeded them, and used handlebars to loop through projects.

---
+++ models/Project.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+
+class Project extends Model {
+
+}
+
+Project.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    link: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    repository: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
+);
+
+module.exports = Project;
